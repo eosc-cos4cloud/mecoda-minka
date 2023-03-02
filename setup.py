b2af59542d2602c86455d96b02467663f1d3b8e0
anomalia <anavgo@gmail.com>
2023-03-02 16:25:32 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="mecoda-minka",
-    version="1.0.0",
+    version="1.0.1",
     description="Library to download information collected in Minka API.",
     author="Ana Alvarez",
     author_email="anomalia@disroot.org",
