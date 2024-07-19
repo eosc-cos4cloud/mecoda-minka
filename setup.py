7d3abebe53c43d909f601e662280bf9561aff86e
anomalia <anavgo@gmail.com>
2024-07-19 12:58:07 +0200

    bump: from 1.7.0 -> 1.7.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="mecoda-minka",
-    version="1.7.0",
+    version="1.7.1",
     description="Library to download information using Minka API.",
     author="Ana Alvarez",
     author_email="ana.alvarez@icm.csic.es",
